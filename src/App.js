c60752d7c4a04234233935c6450da15fec043ed6
charlotte divine <divinemaina@gmail.com>
2022-11-02 14:27:47 -0700

    add sign in page and its styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import SignUp from './components/signup/SignUp';
+import SignIn from './components/signin/SignIn';
 import './App.css';
 
 const App = () => (
@@ -9,6 +10,7 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/SignIn" element={<SignIn />} />
 
     </Routes>
   </div>
