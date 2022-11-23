c93b30bb91416248849fd3bcf6860daaf16a3fcd
charlotte divine <divinemaina@gmail.com>
2022-11-22 18:53:36 -0800

    add main page contents

--- src/components/main/MainPage.js
+++ src/components/main/MainPage.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Nav from '../nav/Nav';
 import './MainPage.css';
+import villa2 from '../../assets/images/villa2.jpg';
+import villa5 from '../../assets/images/villa5.jpg';
+import villa4 from '../../assets/images/villa4.jpg';
+import villa6 from '../../assets/images/villa6.jpg';
 
 const Main = () => (
   <div>
     <Nav />
     Main
+    <div className="mainPage">
+      <img src={villa2} alt="villa advert" />
+      <img src={villa6} alt="villa advert" />
+      <img src={villa5} alt="villa advert" />
+      <img src={villa4} alt="villa advert" />
+    </div>
   </div>
 );
 
