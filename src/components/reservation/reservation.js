043a8cfdffb3ff45aeece6a74b7dcfd90cf9a0a4
charlotte divine <divinemaina@gmail.com>
2022-11-30 21:28:08 -0800

    add reservation page

---
+++ src/components/reservation/reservation.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { FiSettings } from 'react-icons/fi';
+import { AiFillRightCircle } from 'react-icons/ai';
+import './reservation.css';
+
+const Reservation = () => {
+
+  return (
+    <div className="reservations-container">
+      <div className="reservation-background"></div>
+      <div className="overlay"></div>
+      <div className="reservation-content">
+        <h1 className="reservation-heading">Reservations</h1>
+
+        <form className="reservation-form" action="">
+          <input
+            type="date"
+            aria-label="Date"
+            name=""
+            id="date"
+            className="reservation-input"
+            placeholder=""
+            required
+          />
+          <input
+            type="number"
+            name=""
+            id="persons_number"
+            className="reservation-input"
+            min="1"
+            placeholder="Number of Persons"
+            required
+          />
+          <select
+            id="tour_id"
+            className="reservation-input"
+          >
+            <option key="option">--Option--</option>
+            {/* {Store.map((element) => (
+              <option key={element.id} value={element.id}>
+                {element.name}
+              </option>
+            ))} */}
+          </select>
+        </form>
+
+        <button className="reserve-button" type="submit">
+          <FiSettings className="reserve-symbol-1" />
+          Reserve
+          <AiFillRightCircle className="reserve-symbol-2" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Reservation;
